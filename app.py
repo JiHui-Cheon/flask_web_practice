f213c62d60f04b0fba228c3ee3957ab9eee230c3
Jihui_Cheon <cheon3281@gmail.com>
2021-04-23 01:40:03 +0900

    페이지 완성!

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from data import Articles
+import pymysql
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
+
 app.debug = True
+
+
+db = pymysql.connect(
+  host='localhost',
+  port = 3306,
+  user = 'root',
+  passwd = '2643',
+  db = 'practice'
+)
+
 
 @app.route('/', methods = ['GET'])
 def index():
@@ -11,21 +24,81 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template("about.html", hello = "지희쨩")
+    return render_template("about.html", hello = "안녕")
 
 @app.route('/articles')
 def articles():
-    articles = Articles()
+    cursor = db.cursor()
+    sql = 'SELECT * FROM topic;'
+    cursor.execute(sql)
+    topics = cursor.fetchall()
+    db.commit()
+    # articles = Articles()
     # print(articles[0]['author'])
-    return render_template("articles.html", articles = articles)
+
+    return render_template("articles.html", articles = topics)
 
 @app.route('/article/<int:id>')
 def article(id):
-    articles = Articles()
-    article = articles[id-1]
-    print("articles[id-1]")
-    return render_template("article.html", article = article)
+    cursor = db.cursor()
+    sql = 'SELECT * FROM topic WHERE id={}'.format(id)
+    cursor.execute(sql)
+    topic = cursor.fetchonte()
+    print(topic)
+    # articles = Articles()
+    # article = articles[id-1]
+    # print("articles[id-1]")
+    return render_template("article.html", article = topic)
 
+@app.route('/add_articles', methods = ["GET","POST"])
+def add_articles():
+    cursor = db.cursor()
+    if request.method == "POST":
+        author = request.form['author']
+        title = request.form['title']
+        desc = request.form['desc']
+
+        sql = "INSERT INTO `topic` (`title`, `desc`, `author`) VALUES (%s, %s, %s);"
+        input_data = [title, desc, author ]
+        print(request.form['desc'])
+
+        cursor.execute(sql, input_data)
+        db.commit()
+        return redirect("/articles")
+    else:
+        return render_template("add_articles.html")
+
+@app.route('/edit/<int:id>', methods=["GET", "POST"])
+def edit(id):
+    cursor = db.cursor()
+    if request.method == "POST":
+        title = request.form['title']
+        desc = request.form['desc']
+        author = request.form['author']
+        print(id)
+        sql = 'UPDATE topic SET title = %s, body = %s, author = %s WHERE id = {};'.format(id)
+        input_data = [title, desc, author]
+        cursor.execute(sql, input_data)
+        db.commit()
+        print(request.form['title'])
+        return redirect('/articles')
+    
+    else:
+        sql = "SELECT * FROM topic WHERE id = {}".format(id) # db아이디임.
+        cursor.execute(sql)
+        topic = cursor.fetchone()
+        # print(topic[1])
+        return render_template("edit_article.html", article = topic)
+
+@app.route('/delete/<int:id>', methods = ["POST"])
+def delete(id):
+    cursor = db.cursor()
+    sql = 'DELETE FROM topic WHERE id = {};'.format(id) 
+    cursor.execute(sql)
+    db.commit()
+    return redirect("/articles")
+    
+                   
 
 if __name__ == '__main__':
     app.run()
